b574a180f7f81ee10dab9ed2ee7cbb0ada46aa72
Tetiana <muzychko.tatyana@gmail.com>
2025-03-11 12:03:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+leftBorderWidth = 5;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj);
+
+var name = 'Taty';
+console.log(name);
+
+{
+    let result = 50;
+}
+console.log(result);
